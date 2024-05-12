df177be01f854559952aab0b96824da6d65ac2ae
Wijekoon <sarinduwijekoon@gmail.com>
2024-05-12 17:43:44 +0530

    add some css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,101 +1,139 @@
-import React, { useState } from 'react';
-import { Typography, Form, Input, Button, message } from 'antd';
-import { UserOutlined, LockOutlined, GoogleOutlined } from '@ant-design/icons';
-import { Navigate, Link } from 'react-router-dom';
-import { doSignInWithEmailAndPassword, doSignInWithGoogle } from '../firebase/auth';
-import { useAuth } from '../contexts/AuthContext';
+import React, { useState } from "react";
+import { Typography, Form, Input, Button, message } from "antd";
+import { UserOutlined, LockOutlined, GoogleOutlined } from "@ant-design/icons";
+import { Navigate, Link } from "react-router-dom";
+import {
+  doSignInWithEmailAndPassword,
+  doSignInWithGoogle,
+} from "../firebase/auth";
+import { useAuth } from "../contexts/AuthContext";
 
 const { Title, Paragraph } = Typography;
 
 const Login = () => {
-    const { userLoggedIn } = useAuth();
-    const [isSigningIn, setIsSigningIn] = useState(false);
+  const { userLoggedIn } = useAuth();
+  const [isSigningIn, setIsSigningIn] = useState(false);
 
-    const onFinish = async (values) => {
-        setIsSigningIn(true);
-        try {
-            await doSignInWithEmailAndPassword(values.email, values.password);
-        } catch (error) {
-            message.error(error.message);
-        } finally {
-            setIsSigningIn(false);
-        }
-    };
-
-    const onGoogleSignIn = async () => {
-        setIsSigningIn(true);
-        try {
-            await doSignInWithGoogle();
-        } catch (error) {
-            message.error(error.message);
-        } finally {
-            setIsSigningIn(false);
-        }
-    };
-
-    if (userLoggedIn) {
-        return <Navigate to="/" replace={true} />;
+  const onFinish = async (values) => {
+    setIsSigningIn(true);
+    try {
+      await doSignInWithEmailAndPassword(values.email, values.password);
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      setIsSigningIn(false);
     }
+  };
 
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-            <div style={{ maxWidth: 400 }}>
-                <Title level={2} style={{ textAlign: 'center' }}>Welcome Back</Title>
-                <Form
-                    name="login_form"
-                    initialValues={{ remember: true }}
-                    onFinish={onFinish}
-                    style={{ marginTop: 30 }}
-                >
-                    <Form.Item
-                        name="email"
-                        rules={[{ required: true, message: 'Please enter your email!' }]}
-                    >
-                        <Input prefix={<UserOutlined />} placeholder="Email" size="large" />
-                    </Form.Item>
+  const onGoogleSignIn = async () => {
+    setIsSigningIn(true);
+    try {
+      await doSignInWithGoogle();
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      setIsSigningIn(false);
+    }
+  };
 
-                    <Form.Item
-                        name="password"
-                        rules={[{ required: true, message: 'Please enter your password!' }]}
-                    >
-                        <Input.Password prefix={<LockOutlined />} placeholder="Password" size="large" />
-                    </Form.Item>
+  if (userLoggedIn) {
+    return <Navigate to="/" replace={true} />;
+  }
 
-                    <Form.Item>
-                        <Button
-                            type="primary"
-                            htmlType="submit"
-                            style={{ width: '100%' }}
-                            loading={isSigningIn}
-                            size="large"
-                        >
-                            Sign In
-                        </Button>
-                    </Form.Item>
-                </Form>
-                <Paragraph style={{ textAlign: 'center' }}>
-                   Don't have an account?   <Link to={'/register'}>Sign up</Link>
-                </Paragraph>
-                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <div style={{ flex: 1, borderBottom: '1px solid #ccc', marginRight: 10 }}></div>
-                    <div style={{ fontSize: 14, fontWeight: 'bold' }}>OR</div>
-                    <div style={{ flex: 1, borderBottom: '1px solid #ccc', marginLeft: 10 }}></div>
-                </div>
-                <Button
-                    style={{ width: '100%', marginTop: 10 }}
-                    loading={isSigningIn}
-                    onClick={onGoogleSignIn}
-                    size="large"
-                    icon={<GoogleOutlined />}
-                >
-                    Continue with Google
-                </Button>
-            </div>
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        backgroundImage: `url('/sunrise-earth-s-horizon-observed-from-outer-space.jpg')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div
+        style={{
+          maxWidth: 400,
+          backgroundColor: "rgba(0, 0, 0, 0.25)",
+          backdropFilter: "blur(5px)",
+          WebkitBackdropFilter: "blur(5px)",
+          padding: "80px",
+          borderRadius: "10px",
+        }}
+      >
+        <Title level={2} style={{ textAlign: "center",color:"white" }}>
+          Welcome Back
+        </Title>
+        <Form
+          name="login_form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          style={{ marginTop: 30 }}
+        >
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: "Please enter your email!" }]}
+          >
+            <Input prefix={<UserOutlined />} placeholder="Email" size="large" style={{boxShadow: "0 4px 8px rgba(0, 0, 0, 0.6)",}}/>
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please enter your password!" }]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              placeholder="Password"
+              size="large"
+              style={{boxShadow: "0 4px 8px rgba(0, 0, 0, 0.6)",}}
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{ width: "100%" ,boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)"}}
+              loading={isSigningIn}
+              size="large"
+            >
+              Sign In
+            </Button>
+          </Form.Item>
+        </Form>
+        <Paragraph style={{ textAlign: "center", color:"white" }}>
+          Don't have an account? <Link to={"/register"} style={{color:"lightblue"}}>Sign up</Link>
+        </Paragraph>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{ flex: 1, borderBottom: "1px solid #ccc", marginRight: 10 }}
+          ></div>
+          <div style={{ fontSize: 14, fontWeight: "bold", color:"white" }}>OR</div>
+          <div
+            style={{ flex: 1, borderBottom: "1px solid #ccc", marginLeft: 10 }}
+          ></div>
         </div>
-    );
+        <Button
+          style={{ width: "100%", marginTop: 10 }}
+          loading={isSigningIn}
+          onClick={onGoogleSignIn}
+          size="large"
+          icon={<GoogleOutlined />}
+        >
+          Continue with Google
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
-
 
 // style={{ backgroundColor: 'rgba(255, 255, 255, 0.1)', backdropFilter: 'blur(10px)' }}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,105 +1,153 @@
-import React, { useState } from 'react';
-import { Typography, Form, Input, Button, message } from 'antd';
-import { Navigate, Link } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
-import { doCreateUserWithEmailAndPassword } from '../firebase/auth';
+import React, { useState } from "react";
+import { Typography, Form, Input, Button, message } from "antd";
+import { Navigate, Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { doCreateUserWithEmailAndPassword } from "../firebase/auth";
+import { UserOutlined, LockOutlined, GoogleOutlined } from "@ant-design/icons";
 
 const { Title } = Typography;
 
 const Register = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [isRegistering, setIsRegistering] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const { userLoggedIn } = useAuth();
+  const { userLoggedIn } = useAuth();
 
-    const onFinish = async () => {
-        if (!isRegistering) {
-            setIsRegistering(true);
-            try {
-                await doCreateUserWithEmailAndPassword(email, password);
-            } catch (error) {
-                setErrorMessage(error.message);
-            } finally {
-                setIsRegistering(false);
-            }
-        }
-    };
-
-    if (userLoggedIn) {
-        return <Navigate to="/home" replace={true} />;
+  const onFinish = async () => {
+    if (!isRegistering) {
+      setIsRegistering(true);
+      try {
+        await doCreateUserWithEmailAndPassword(email, password);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsRegistering(false);
+      }
     }
+  };
 
-    return (
-        <>
-            <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-                <div style={{ maxWidth: 400 }}>
-                    <Title level={2} style={{ textAlign: 'center' }}>Create a New Account</Title>
-                    <Form
-                        name="register_form"
-                        initialValues={{ remember: true }}
-                        onFinish={onFinish}
-                        style={{ marginTop: 30 }}
-                    >
-                        <Form.Item
-                            name="email"
-                            rules={[{ required: true, message: 'Please enter your email!' }]}
-                        >
-                            <Input placeholder="Email" size="large" onChange={(e) => setEmail(e.target.value)} />
-                        </Form.Item>
+  if (userLoggedIn) {
+    return <Navigate to="/home" replace={true} />;
+  }
 
-                        <Form.Item
-                            name="password"
-                            rules={[{ required: true, message: 'Please enter your password!' }]}
-                        >
-                            <Input.Password placeholder="Password" size="large" onChange={(e) => setPassword(e.target.value)} />
-                        </Form.Item>
+  return (
+    <>
+      <main
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+          backgroundImage: `url('/sunrise-earth-s-horizon-observed-from-outer-space.jpg')`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div
+          style={{
+            maxWidth: 400,
+            backgroundColor: "rgba(0, 0, 0, 0.25)",
+            backdropFilter: "blur(5px)",
+            WebkitBackdropFilter: "blur(5px)",
+            padding: "80px",
+            borderRadius: "10px",
+          }}
+        >
+          <Title level={2} style={{ textAlign: "center", color: "white" }}>
+            Create a New Account
+          </Title>
+          <Form
+            name="register_form"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            style={{ marginTop: 30 }}
+          >
+            <Form.Item
+              name="email"
+              rules={[{ required: true, message: "Please enter your email!" }]}
+            >
+              <Input
+                prefix={<UserOutlined />}
+                placeholder="Email"
+                size="large"
+                onChange={(e) => setEmail(e.target.value)}
+                style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.6)" }}
+              />
+            </Form.Item>
 
-                        <Form.Item
-                            name="confirmPassword"
-                            rules={[
-                                { required: true, message: 'Please confirm your password!' },
-                                ({ getFieldValue }) => ({
-                                    validator(_, value) {
-                                        if (!value || getFieldValue('password') === value) {
-                                            return Promise.resolve();
-                                        }
-                                        return Promise.reject(new Error('The two passwords do not match!'));
-                                    },
-                                }),
-                            ]}
-                        >
-                            <Input.Password placeholder="Confirm Password" size="large" onChange={(e) => setConfirmPassword(e.target.value)} />
-                        </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please enter your password!" },
+              ]}
+            >
+              <Input.Password
+                prefix={<LockOutlined />}
+                placeholder="Password"
+                size="large"
+                onChange={(e) => setPassword(e.target.value)}
+                style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.6)" }}
+              />
+            </Form.Item>
 
-                        {errorMessage && (
-                            <span style={{ color: 'red', fontWeight: 'bold' }}>{errorMessage}</span>
-                        )}
+            <Form.Item
+              name="confirmPassword"
+              rules={[
+                { required: true, message: "Please confirm your password!" },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue("password") === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error("The two passwords do not match!")
+                    );
+                  },
+                }),
+              ]}
+            >
+              <Input.Password
+                prefix={<LockOutlined />}
+                placeholder="Confirm Password"
+                size="large"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.6)" }}
+              />
+            </Form.Item>
 
-                        <Form.Item>
-                            <Button
-                                type="primary"
-                                htmlType="submit"
-                                style={{ width: '100%' }}
-                                loading={isRegistering}
-                                size="large"
-                            >
-                                {isRegistering ? 'Signing Up...' : 'Sign Up'}
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                    <div style={{ textAlign: 'center' }}>
-                        <p>
-                            Already have an account?{' '}
-                            <Link to="/">Login</Link>
-                        </p>
-                    </div>
-                </div>
-            </main>
-        </>
-    );
+            {errorMessage && (
+              <span style={{ color: "red", fontWeight: "bold" }}>
+                {errorMessage}
+              </span>
+            )}
+
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ width: "100%" ,boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)"}}
+                loading={isRegistering}
+                size="large"
+              >
+                {isRegistering ? "Signing Up..." : "Sign Up"}
+              </Button>
+            </Form.Item>
+          </Form>
+          <div style={{ textAlign: "center", color: "white" }}>
+            <p>
+              Already have an account?{" "}
+              <Link to="/" style={{ color: "lightblue"}}>
+                Login
+              </Link>
+            </p>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Register;
